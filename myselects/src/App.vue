5f84bcefefecfc36612de145c32dc59ba4de87c9
qiu_miaomiao <qmm9678@163.com>
2018-03-15 17:10:48 +0800

    Initial commit

---
+++ myselects/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app"> 
+    <selects></selects>
+  </div>
+</template>
+
+<script>
+  import Selects from './components/select';
+  export default {
+    name: 'App',
+    components:{Selects}
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ myselects/src/components/select.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="selects">
+		<select v-model="select1" id="select1">
+			<option value="" selected>请选择</option>
+			<option v-for="item in options" :value="item" v-bind:disabled="item == select2||item == select3">{{item}}</option>
+		</select>
+		<select v-model="select2" id="select2">
+			<option value="" selected>请选择</option>
+			<option v-for="item in options" :value="item" v-bind:disabled="item == select1||item == select3">{{item}}</option>
+		</select>
+		<select v-model="select3" id="select3">
+			<option value="" selected>请选择</option>
+			<option v-for="item in options" :value="item" v-bind:disabled="item == select2||item == select1">{{item}}</option>
+		</select>
+	</div>
+</template>
+
+<script>
+	export default{
+		data: function(){
+			return{
+				options:[1,2,3,4,5,6,7,8,9,10],
+				select1:"",
+				select2:"",
+				select3:""
+			}
+		}
+	}
+</script>
